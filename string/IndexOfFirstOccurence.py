6145889a94f8c7caf1c215e947ac0b1da34451e9
Your Name <youremail@example.com>
2025-11-12 15:14:56 +0530

    IndexOfFirstOccurence

---
+++ string/IndexOfFirstOccurence.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
+    
+obj = Solution()
+print(obj.strStr("sadbutsad","sad"))
